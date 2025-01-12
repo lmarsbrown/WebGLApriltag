2b4f41c0710d72b9da83deb563e715b6ad4963f4
lmarsbrown <lmarsbrown@gmail.com>
2025-01-11 19:18:24 -0800

    Adding a shader i think?

--- script.js
+++ script.js
@@ -32,15 +32,49 @@ var downsizeProgram = createShaderProgram(generic_vs_code,
     }
     `
 );
+let downsizeInSizeLoc = gl.getUniformLocation(downsizeProgram,"inSize");
+let downsizeScaleLoc  = gl.getUniformLocation(downsizeProgram,"scale");
 
 
-let mainBuffer;
-load_img("apriltags_30mm.png",(data)=>{
+
+
+/**
+ * 
+ * @param {GPUImage} input 
+ * @param {GPUImage} output 
+ */
+function downsize(input,output,scale)
+{
+    gl.useProgram(downsizeProgram); 
+    gl.viewport(0,0,output.width,output.height)
+
+    // if(output.width*scale != input.width || output.height*scale != input.height)
+    // {
+    //     throw "input output size mismatch";
+    // }
+
+    gl.uniform2i(downsizeInSizeLoc,input.width,input.height);
+    gl.uniform1i(downsizeScaleLoc,scale);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D,input.frontTex);
+    gl.bindFramebuffer(gl.FRAMEBUFFER,output.backFb);
+    gl.drawArrays(gl.TRIANGLES,0,3);
+    output.swapBuffers();
+}
+
+var mainBuffer;
+var scaledBuffer;
+
+load_img("apriltag_setup.jpg",(data)=>{
     can.width = data.width;
     can.height = data.height;
 
     mainBuffer = new GPUImage(data.width,data.height);
+    scaledBuffer = new GPUImage(Math.floor(data.width/4),Math.floor(data.height/4))
 
     mainBuffer.writeImage(data);
-    render(mainBuffer);
+    downsize(mainBuffer,scaledBuffer,4);
+
+    render(scaledBuffer);
 })
